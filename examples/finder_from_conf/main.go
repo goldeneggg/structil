91c96977efc6023b1c4dcef1e3d41b244e641640
goldeneggg <jpshadowapps@gmail.com>
2019-06-11 23:51:18 +0900

    add FinderKeys and exmaples

--- examples/finder_from_conf/main.go
+++ examples/finder_from_conf/main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-
-	"github.com/spf13/viper"
 
 	"github.com/goldeneggg/structil"
 )
-
-const (
-	DefaultYAML = `
-Defs:
-  - Company:
-    - Group:
-      - Boss
-    - Period
-  - Name
-  - Age
-`
-)
-
-type Definition struct {
-	Defs []interface{}
-}
 
 type person struct {
 	Name    string
@@ -78,64 +59,30 @@ func main() {
 		},
 	}
 
-	viper.SetConfigType("yaml")
-	viper.ReadConfig(bytes.NewBuffer([]byte(DefaultYAML)))
-
-	var def Definition
-	viper.Unmarshal(&def)
-	fmt.Printf("%+v\n", def.Defs)
+	fks, err := structil.NewFinderKeysFromConf("examples/finder_from_conf/", "ex_json")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("fks.Keys(json): %#v\n", fks.Keys())
 
 	finder, err := structil.NewFinder(i)
 	if err != nil {
 		panic(err)
 	}
 
-	hoge := &hoge{keys: make([]string, 0, len(def.Defs)+1)}
-	for i, d := range def.Defs {
-		fmt.Printf("----- %d is string. def: %s\n", i, d)
-		hoge.setKeyRecursive(d, "")
-	}
-	fmt.Printf("hoge.keys: %+v\n", hoge.keys)
+	finder = finder.FromKeys(fks)
 
-	m, err := finder.
-		Find("Name").
-		Into("Company").Find("Address").
-		Into("Company", "Group").Find("Name", "Boss").
-		ToMap()
+	m, err := finder.ToMap()
+	fmt.Printf("Found Map(json): %#v, err: %v\n", m, err)
+
+	fks, err = structil.NewFinderKeysFromConf("examples/finder_from_conf/", "ex_yml")
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("fks.Keys(yml): %#v\n", fks.Keys())
 
-	fmt.Printf("Found Map: %#v\n", m)
-}
+	finder = finder.Reset().FromKeys(fks)
 
-func (h *hoge) setKeyRecursive(d interface{}, prefix string) {
-	var res string
-
-	switch t := d.(type) {
-	case string:
-		res = t
-		if prefix != "" {
-			res = prefix + "." + res
-		}
-		h.keys = append(h.keys, res) // set here
-	case map[interface{}]interface{}:
-		var nk string
-		var nd interface{}
-		for key, value := range t {
-			nk = key.(string)
-			if prefix != "" {
-				nk = prefix + "." + nk
-			}
-			nd = value
-			break
-		}
-		h.setKeyRecursive(nd, nk)
-	case []interface{}:
-		for _, value := range t {
-			h.setKeyRecursive(value, prefix)
-		}
-	default:
-		panic(fmt.Sprintf("other. def: %#v, prefix: %s\n", t, prefix))
-	}
+	m, err = finder.ToMap()
+	fmt.Printf("Found Map(yml): %#v, err: %v\n", m, err)
 }
---
+++ finder_keys.go
@@ -0,0 +1,122 @@
+package structil
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type FinderKeys struct {
+	keys []string
+}
+
+type confKeys struct {
+	Keys []interface{}
+}
+
+func NewFinderKeysFromConf(dir string, baseName string) (*FinderKeys, error) {
+	viper.SetConfigName(baseName)
+	viper.AddConfigPath(dir)
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var ck confKeys
+	if err := viper.Unmarshal(&ck); err != nil {
+		return nil, err
+	}
+
+	return newFinderKeysFromConf(ck)
+}
+
+func newFinderKeysFromConf(ck confKeys) (*FinderKeys, error) {
+	if len(ck.Keys) == 0 {
+		return nil, fmt.Errorf("failed to parse or no keys exist in file")
+	}
+
+	fks := &FinderKeys{keys: make([]string, 0, len(ck.Keys)+1)}
+
+	var err error
+	for _, ckk := range ck.Keys {
+		err = fks.addRecursive(ckk, "")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return fks, nil
+}
+
+func (fks *FinderKeys) addRecursive(key interface{}, prefix string) error {
+	var res string
+
+	switch t := key.(type) {
+	case string:
+		res = t
+		if prefix != "" {
+			res = prefix + defaultSep + res
+		}
+		fks.keys = append(fks.keys, res) // set here
+	case map[string]interface{}:
+		var nk string
+		var nd interface{}
+		for key, value := range t {
+			nk = key
+			if prefix != "" {
+				nk = prefix + "." + nk
+			}
+			nd = value
+			break
+		}
+		err := fks.addRecursive(nd, nk)
+		if err != nil {
+			return err
+		}
+	case map[interface{}]interface{}:
+		var nk string
+		var nd interface{}
+		for key, value := range t {
+			nk = key.(string)
+			if prefix != "" {
+				nk = prefix + "." + nk
+			}
+			nd = value
+			break
+		}
+		err := fks.addRecursive(nd, nk)
+		if err != nil {
+			return err
+		}
+	case []interface{}:
+		var err error
+		for _, value := range t {
+			err = fks.addRecursive(value, prefix)
+			if err != nil {
+				return err
+			}
+		}
+	default:
+		return fmt.Errorf("unsupported type: %#v, prefix: %s", t, prefix)
+	}
+
+	return nil
+}
+
+func (fks *FinderKeys) Len() int {
+	return len(fks.keys)
+}
+
+func (fks *FinderKeys) Keys() []string {
+	return fks.keys
+}
+
+func (fks *FinderKeys) intosAndFinds(i int) ([]string, []string) {
+	s := strings.Split(fks.keys[i], defaultSep)
+	if len(s) == 1 {
+		return nil, s[:1]
+	}
+
+	return s[0 : len(s)-1], s[len(s)-1:]
+}
