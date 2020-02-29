24e7a5760d4b68229754cdf64aede2c78235ddb0
goldeneggg <jpshadowapps@gmail.com>
2020-02-29 17:55:29 +0900

    bump up VERSIOn

---
+++ version_test.go
@@ -0,0 +1,15 @@
+package structil_test
+
+import (
+	"testing"
+
+	. "github.com/goldeneggg/structil"
+)
+
+func TestVersion(t *testing.T) {
+	exp := "0.4.0"
+
+	if VERSION != exp {
+		t.Errorf("expected: %#v, but actual: %#v", exp, VERSION)
+	}
+}
