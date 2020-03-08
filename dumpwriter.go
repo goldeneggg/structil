f9d2261172924a913eab65e5c9cfe4ce70f799b9
goldeneggg <jpshadowapps@gmail.com>
2020-03-09 05:29:32 +0900

    move package

--- dumpwriter.go
+++ dumpwriter.go
@@ -1,4 +1,4 @@
-package dumpwriter
+package structil
 
 import (
 	"fmt"
@@ -29,7 +29,7 @@ type Param struct {
 }
 
 // New returns a new default DumpWriter that wraps tabwriter.
-func New() DumpWriter {
+func NewDumpWriter() DumpWriter {
 	dwp := &Param{
 		MinWidth: 0,
 		TabWidth: 4,
@@ -38,11 +38,11 @@ func New() DumpWriter {
 		Flags:    0,
 	}
 
-	return NewWithSetupInfo(dwp, os.Stdout)
+	return NewDumpWriterWithSetupInfo(dwp, os.Stdout)
 }
 
-// NewWithSetupInfo returns a new default DumpWriter that wraps the Writer assigned by "wrap" arg.
-func NewWithSetupInfo(dwp *Param, wrap io.Writer) DumpWriter {
+// NewDumpWriterWithSetupInfo returns a new default DumpWriter that wraps the Writer assigned by "wrap" arg.
+func NewDumpWriterWithSetupInfo(dwp *Param, wrap io.Writer) DumpWriter {
 	dw := &dwImpl{}
 
 	dw.tw = tabwriter.NewWriter(
