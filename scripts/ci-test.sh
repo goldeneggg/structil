a0a170c60014aa275ccd191f4ab9f52ca5cff6e4
goldeneggg <jpshadowapps@gmail.com>
2020-08-12 10:51:43 +0900

    add comment

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -5,7 +5,9 @@ source scripts/_prepare.sh
 
 echo "" > ${BASE_DIR}/coverage.txt
 for d in $(${MYDIR}/_packages.sh); do
+  # Note: must use "go" instead of "${LOCAL_GO}" because this script is executed on CI
   go test -race -parallel 2 -coverprofile=profile.out -covermode=atomic $d
+
   if [ -f profile.out ]; then
     cat profile.out >> ${BASE_DIR}/coverage.txt
     rm profile.out
