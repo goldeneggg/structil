e635f402eb7f251a0a0b9adf87ad55c977463d1b
goldeneggg <jpshadowapps@gmail.com>
2020-08-13 13:10:31 +0900

    fix example

--- examples/dystruct_unknown_json_decode/main.go
+++ examples/dystruct_unknown_json_decode/main.go
@@ -56,66 +56,64 @@ func singleJSON() {
 }
 
 func arrayJSON() {
-	unknownFormatJSON := []byte(`
-[
-	{
-		"string_field":"かきくけこ",
-		"int_field":45678,
-		"float32_field":9.876,
-		"bool_field":false,
-		"struct_ptr_field":{
-			"key":"hugakey",
-			"value":"hugavalue"
-		},
-		"array_string_field":[
-			"array_str_1",
-			"array_str_2"
-		],
-		"array_struct_field":[
-			{
-				"kkk":"kkk1",
-				"vvvv":"vvv1"
-			},
-			{
-				"kkk":"kkk2",
-				"vvvv":"vvv2"
-			},
-			{
-				"kkk":"kkk3",
-				"vvvv":"vvv3"
-			}
-		]
-	},
-	{
-		"string_field":"さしすせそ",
-		"int_field":7890,
-		"float32_field":4.99,
-		"bool_field":true,
-		"struct_ptr_field":{
-			"key":"hugakeyXXX",
-			"value":"hugavalueXXX"
-		},
-		"array_string_field":[
-			"array_str_111",
-			"array_str_222"
-		],
-		"array_struct_field":[
-			{
-				"kkk":"kkk99",
-				"vvvv":"vvv99"
-			},
-			{
-				"kkk":"kkk999",
-				"vvvv":"vvv999"
-			},
-			{
-				"kkk":"kkk9999",
-				"vvvv":"vvv9999"
-			}
-		]
-	}
-]
-`)
+	unknownFormatJSON := []byte(`[
+  {
+    "string_field": "かきくけこ",
+    "int_field": 45678,
+    "float32_field": 9.876,
+    "bool_field": false,
+    "struct_ptr_field": {
+      "key": "hugakey",
+      "value": "hugavalue"
+    },
+    "array_string_field": [
+      "array_str_1",
+      "array_str_2"
+    ],
+    "array_struct_field": [
+      {
+        "kkk": "kkk1",
+        "vvvv": "vvv1"
+      },
+      {
+        "kkk": "kkk2",
+        "vvvv": "vvv2"
+      },
+      {
+        "kkk": "kkk3",
+        "vvvv": "vvv3"
+      }
+    ]
+  },
+  {
+    "string_field": "さしすせそ",
+    "int_field": 7890,
+    "float32_field": 4.99,
+    "bool_field": true,
+    "struct_ptr_field": {
+      "key": "hugakeyXXX",
+      "value": "hugavalueXXX"
+    },
+    "array_string_field": [
+      "array_str_111",
+      "array_str_222"
+    ],
+    "array_struct_field": [
+      {
+        "kkk": "kkk99",
+        "vvvv": "vvv99"
+      },
+      {
+        "kkk": "kkk999",
+        "vvvv": "vvv999"
+      },
+      {
+        "kkk": "kkk9999",
+        "vvvv": "vvv9999"
+      }
+    ]
+  }
+]`)
 
 	intf, err := dynamicstruct.JSONToDynamicStructInterface(unknownFormatJSON)
 	if err != nil {
