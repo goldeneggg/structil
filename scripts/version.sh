e2164e9f60c510fb50d79cc1ad4effc7d4197d44
goldeneggg <jpshadowapps@gmail.com>
2021-10-01 17:02:46 +0900

    replace which from "command -v"

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,7 +4,7 @@ set -eu
 # shellcheck disable=SC1091
 source scripts/_prepare.sh
 
-GREP=$(which grep)
-SED=$(which sed)
+GREP=$(command -v grep)
+SED=$(command -v sed)
 
 ${GREP} "const VERSION" "${BASE_DIR}"/version.go | ${SED} -e 's/const VERSION = //g' | ${SED} -e 's/\"//g'
