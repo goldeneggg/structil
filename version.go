8e887e12350f92f1ab47dd0bb3f5c57a23897396
goldeneggg <jpshadowapps@gmail.com>
2022-07-27 01:45:18 +0900

    bump up to 0.9.1

--- version.go
+++ version.go
@@ -1,4 +1,4 @@
 package structil
 
 // VERSION says my version number
-const VERSION = "0.9.0"
+const VERSION = "0.9.1"
--- version_test.go
+++ version_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestVersion(t *testing.T) {
-	exp := "0.9.0"
+	exp := "0.9.1"
 
 	if VERSION != exp {
 		t.Errorf("expected: %#v, but actual: %#v", exp, VERSION)
