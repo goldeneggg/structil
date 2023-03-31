16c9497273587372ce593e1879b2d5b7a2b8469c
goldeneggg <jpshadowapps@gmail.com>
2023-03-31 15:11:57 +0900

    WIP update cmd

--- cmd/strucmd/main.go
+++ cmd/strucmd/main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+
+	"github.com/goldeneggg/structil/dynamicstruct/decoder"
 )
 
 func main() {
-	fmt.Println("NOT IMPLEMENTED YET")
+	// fmt.Println("NOT IMPLEMENTED YET")
+
+	f, err := os.Open("demo.json")
+	if err != nil {
+		panic(err)
+	}
+
+	b, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+
+	dec, err := decoder.FromJSON(b)
+	if err != nil {
+		panic(err)
+	}
+
+	nest := true
+	useTag := true
+	ds, err := dec.DynamicStruct(nest, useTag)
+	if err != nil {
+		panic(err)
+	}
+
+	// Print struct definition from DynamicStruct
+	fmt.Println(ds.Definition())
 }
